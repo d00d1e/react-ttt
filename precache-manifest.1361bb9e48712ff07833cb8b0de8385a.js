480752bdbcf2739d1895e13262d28834f4e1eeb0
d00d1e <lee.julianay@gmail.com>
2021-02-09 19:50:11 -0800

    Updates

---
+++ precache-manifest.1361bb9e48712ff07833cb8b0de8385a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b0ae924ed0556a18e4a7d3b5f61bb68",
+    "url": "/react-ttt/index.html"
+  },
+  {
+    "revision": "86255ac2eaed93fd3ea2",
+    "url": "/react-ttt/static/css/main.fc70c78d.chunk.css"
+  },
+  {
+    "revision": "8ae3dc0b306aee8a6742",
+    "url": "/react-ttt/static/js/2.0a3f21ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-ttt/static/js/2.0a3f21ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86255ac2eaed93fd3ea2",
+    "url": "/react-ttt/static/js/main.e6e89648.chunk.js"
+  },
+  {
+    "revision": "b223d708855fea33b27c",
+    "url": "/react-ttt/static/js/runtime-main.54b71e18.js"
+  }
+]);
